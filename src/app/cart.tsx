ca4e3e9a1785e6f22df4e2cdd4e4f5f2bbf460f8
Maria Emilia Päivärinta <eemilia.paivarinta@gmail.com>
2025-03-22 10:20:32 +0100

    Create shopping cart icon and functionality to topbar menu to navigate Cart-screen

---
+++ src/app/cart.tsx
@@ -0,0 +1,16 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, Text, Platform } from "react-native";
+
+const CartScreen = () => {
+    return (
+        <View>
+            <Text>cart</Text>
+
+            <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+        </View>
+    );
+
+};
+
+export default CartScreen;
