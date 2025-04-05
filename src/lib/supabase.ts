743315fc3da8567c029e3b9a51847e1c8255f30b
Maria Emilia Päivärinta <eemilia.paivarinta@gmail.com>
2025-04-05 09:38:46 +0200

    Configurate Supabase to the project

---
+++ src/lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://reoffjfmelemkynopgbh.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlb2ZmamZtZWxlbWt5bm9wZ2JoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM4MzcyMjIsImV4cCI6MjA1OTQxMzIyMn0.Z1NKOYHATwWnMT3jVaL7vhen-xFE65fP2N_GFoHAlcw';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
