11b80005913e6e266554af27c9a59e3b20ef05aa
Maria Emilia Päivärinta <eemilia.paivarinta@gmail.com>
2025-04-07 12:22:08 +0200

    Create Product CRUD operation SELECT, to fetch the Pizza listing item data for user side menu from Supabase (add user and admin side)

--- src/app/(admin)/menu/index.tsx
+++ src/app/(admin)/menu/index.tsx
@@ -1,22 +1,33 @@
-import { FlatList, StyleSheet } from 'react-native';
-import { View } from '../../../components/Themed';
+import { ActivityIndicator, FlatList, StyleSheet } from 'react-native';
+import { View, Text } from '../../../components/Themed';
 // Import the instructors own assets
 import products from '@assets/data/products';
 // Import created child component 
 import ProductListItem from '@components/ProductListItem';
+import { useProductList } from '@/src/api/products';
 
 
 
 /*THIS IS THE HOME SCREEN*/
 export default function MenuScreen() {
+
+  const { data: products, error, isLoading } = useProductList();
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  };
+
+  if (error) {
+    return <Text>Failed to fetch products</Text>
+  }
   return (
     <View>
       {/*Flatlist for scrollable list*/}
       <FlatList
         data={products} renderItem={({ item }) => <ProductListItem product={item} />}
         numColumns={2}
-        contentContainerStyle={{gap: 10, padding: 15}}
-        columnWrapperStyle={{gap: 10}}
+        contentContainerStyle={{ gap: 10, padding: 15 }}
+        columnWrapperStyle={{ gap: 10 }}
       />
     </View>
   );
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,9 +5,10 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-// Import cartProvider for passing data
+// Import providers for passing data
 import CartProvider from '../providers/CartProvider';
 import AuthProvider from '../providers/AuthProvider';
+import QueryProvider from '../providers/QueryProvider';
 
 import { useColorScheme } from '../components/useColorScheme';
 
@@ -54,14 +55,16 @@ function RootLayoutNav() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <AuthProvider>
-      <CartProvider>
-        <Stack>
-        <Stack.Screen name="(user)" options={{ headerShown: false }} />
-          <Stack.Screen name="(admin)" options={{ headerShown: false }} />
-          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-          <Stack.Screen name="cart" options={{ presentation: 'modal' }} />
-        </Stack>
-      </CartProvider>
+        <QueryProvider>
+          <CartProvider>
+            <Stack>
+            <Stack.Screen name="(user)" options={{ headerShown: false }} />
+              <Stack.Screen name="(admin)" options={{ headerShown: false }} />
+              <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+              <Stack.Screen name="cart" options={{ presentation: 'modal' }} />
+            </Stack>
+          </CartProvider>
+        </QueryProvider>
       </AuthProvider>
     </ThemeProvider>
   );
